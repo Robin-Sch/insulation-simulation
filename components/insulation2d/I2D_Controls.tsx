951c6e47e40802c89e10b695ceb844e9679db8d8
Robin-Sch <46354497+Robin-Sch@users.noreply.github.com>
2025-05-26 13:16:44 +0200

    Add new layers for 2d insulation simulation

--- components/insulation2d/I2D_Controls.tsx
+++ components/insulation2d/I2D_Controls.tsx
@@ -23,11 +23,87 @@ export default function I2D_Controls({
         key: K,
         value: Insulation2DLayer[K]
     ) => {
-        console.log(index, key, value);
+        specificConfig.layers[index][key] = value;
+        onSpecificChange({ layers: specificConfig.layers });
+    };
+
+    const handleLayerAdd = () => {
+        const selectedMaterial = document.querySelector<HTMLSelectElement>(
+            'select[name="newLayerMaterial"]'
+        )?.value as InsulationType;
+        const selectedThickness =
+            document.querySelector<HTMLInputElement>(
+                'input[name="newLayerThickness"]'
+            )?.valueAsNumber || 50;
+
+        if (!selectedMaterial) return; // No material selected
+
+        const newLayer: Insulation2DLayer = {
+            material: selectedMaterial,
+            thickness: selectedThickness,
+        };
+
+        onSpecificChange({
+            layers: [...specificConfig.layers, newLayer],
+        });
     };
 
     return (
         <>
+            <h2 className="text-xl font-bold mb-4">Add new</h2>
+            <div className="mb-6">
+                <label className="block text-sm font-medium mb-2">
+                    Insulation Type
+                </label>
+                <select
+                    name="newLayerMaterial"
+                    className="w-full p-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+                >
+                    <option value="" disabled selected>
+                        Select material
+                    </option>
+                    {Object.keys(INSULATION_TYPES).map((type) => (
+                        <option
+                            key={type}
+                            value={type}
+                            className="text-gray-800"
+                        >
+                            {type.charAt(0).toUpperCase() + type.slice(1)}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div>
+                <div className="flex justify-between items-center mb-2">
+                    <label className="block text-sm font-medium">
+                        Thickness
+                    </label>
+                    <span className="text-sm font-semibold">50 cm</span>
+                </div>
+                <input
+                    name="newLayerThickness"
+                    type="range"
+                    min="10"
+                    max="100"
+                    step="5"
+                    defaultValue="50"
+                    className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer accent-blue-600"
+                />
+                <div className="flex justify-between text-xs mt-1">
+                    <span>10cm</span>
+                    <span>100cm</span>
+                </div>
+            </div>
+
+            <button
+                onClick={handleLayerAdd}
+                className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded"
+            >
+                Add
+            </button>
+
+            <h2 className="text-xl font-bold mb-4">Modify existing</h2>
             {specificConfig.layers.map((layer, i) => (
                 <div key={`layer-${i}`}>
                     <div className="mb-6">
